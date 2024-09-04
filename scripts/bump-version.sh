7d95d8f66ad092c51fc4e27a2b66ea24fe23565a
RobinGiel <robingiel@gmail.com>
2024-09-04 15:25:13 +0200

    build: bump version to 0.1.3 🚀

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -10,6 +10,7 @@ fi
 
 # Get the current version from package.json
 VERSION=$(grep -oP '"version":\s*"\K[0-9.]+' package.json)
+PACKAGE_NAME=$(grep -oP '"name":\s*"\K[^"]+' package.json)
 
 # Split version into an array
 IFS='.' read -r -a VERSION_PARTS <<< "$VERSION"
@@ -20,8 +21,8 @@ NEW_VERSION="${VERSION_PARTS[0]}.${VERSION_PARTS[1]}.$((VERSION_PARTS[2] + 1))"
 # Replace the version in package.json
 sed -i "s/\"version\": \"$VERSION\"/\"version\": \"$NEW_VERSION\"/" package.json
 
-# Replace the version in package-lock.json
-sed -i "s/\"version\": \"$VERSION\"/\"version\": \"$NEW_VERSION\"/" package-lock.json
+# Replace the version in package-lock.json for the main package
+sed -i "/\"name\": \"$PACKAGE_NAME\"/,/\"version\": \"$VERSION\"/s/\"version\": \"$VERSION\"/\"version\": \"$NEW_VERSION\"/" package-lock.json
 
 # Print the new version
 echo "Bumped version to $NEW_VERSION"
